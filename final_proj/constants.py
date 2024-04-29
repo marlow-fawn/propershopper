d8e805a12bc587a1e110a0d92b07d71ee4725987
Helen <helenlu66@gmail.com>
2024-04-29 11:00:52 -0400

    changed corner of solitude a little

--- final_proj/constants.py
+++ final_proj/constants.py
@@ -2,7 +2,7 @@
 ##some constants that were useful during single agent norm-conforming navigation##
 ##################################################################################
 CORNER_OF_SOLITUDE = [18.1, 22.65]
-CORNER_OF_SOLITUDE2 = [18.1, 23.65]
+CORNER_OF_SOLITUDE2 = [18.5, 23.65]
 cartReturns = [2, 18.5]
 basketReturns = [3.5, 18.5]
 registerReturns_1 = [2, 4.5]
